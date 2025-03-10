807846cb96072fe62220e12896d94be4c0db7e89
Saravana priyan C <c.saravanapriyan@gmail.com>
2025-03-10 20:59:08 +0530

    done

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 
 //frst use the url /posts and then access the / within it
-app.use("/posts", postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
